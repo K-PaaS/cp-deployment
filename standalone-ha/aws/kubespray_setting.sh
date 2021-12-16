6fa40a79a079fa299d7e0461aa6c3df8da7f78a3
Ubuntu <ubuntu@ip-10-0-0-207.ap-northeast-2.compute.internal>
2021-12-16 02:47:14 +0000

    modify kubespray_setting.sh

--- standalone-ha/aws/kubespray_setting.sh
+++ standalone-ha/aws/kubespray_setting.sh
@@ -21,21 +21,21 @@ sed -i "s/{WORKER3_NODE_PRIVATE_IP}/$WORKER3_NODE_PRIVATE_IP/g" inventory/myclus
 
 sed -i "s/{MASTER_NODE_HOSTNAME}/$MASTER1_NODE_HOSTNAME/g" roles/kubernetes-apps/metrics_server/defaults/main.yml
 
-sed -i "s/{MASTER_NODE_PUBLIC_IP}/$MASTER_NODE_PUBLIC_IP/g" roles/kubernetes/control-plane/tasks/kubeadm-setup.yml
+sed -i "s/{MASTER_NODE_PUBLIC_IP}/$MASTER1_NODE_PUBLIC_IP/g" roles/kubernetes/control-plane/tasks/kubeadm-setup.yml
 
 sed -i "s/{LOADBALANCER_VIP}/$LOADBALANCER_VIP/g" inventory/mycluster/group_vars/all/all.yml
 sed -i "s/{ETCD1_NODE_PRIVATE_IP}/$ETCD1_NODE_PRIVATE_IP/g" inventory/mycluster/group_vars/all/all.yml
 sed -i "s/{ETCD2_NODE_PRIVATE_IP}/$ETCD2_NODE_PRIVATE_IP/g" inventory/mycluster/group_vars/all/all.yml
 sed -i "s/{ETCD3_NODE_PRIVATE_IP}/$ETCD3_NODE_PRIVATE_IP/g" inventory/mycluster/group_vars/all/all.yml
 
-sed -i "/kube_node:/,/etcd:/s/$ETCD1_NODE_HOSTNAME//g" inventory/mycluster/hosts.yaml
-sed -i "/kube_node:/,/etcd:/s/$ETCD2_NODE_HOSTNAME//g" inventory/mycluster/hosts.yaml
-sed -i "/kube_node:/,/etcd:/s/$ETCD3_NODE_HOSTNAME//g" inventory/mycluster/hosts.yaml
+declare -a IPS=($MASTER1_NODE_PRIVATE_IP $MASTER2_NODE_PRIVATE_IP $MASTER3_NODE_PRIVATE_IP $ETCD1_NODE_PRIVATE_IP $ETCD2_NODE_PRIVATE_IP $ETCD3_NODE_PRIVATE_IP $WORKER1_NODE_PRIVATE_IP $WORKER2_NODE_PRIVATE_IP $WORKER3_NODE_PRIVATE_IP)
+CONFIG_FILE=inventory/mycluster/hosts.yaml python3 contrib/inventory_builder/inventory.py ${IPS[@]}
+
+sed -i "/kube_node:/,/etcd:/s/$ETCD1_NODE_HOSTNAME://g" inventory/mycluster/hosts.yaml
+sed -i "/kube_node:/,/etcd:/s/$ETCD2_NODE_HOSTNAME://g" inventory/mycluster/hosts.yaml
+sed -i "/kube_node:/,/etcd:/s/$ETCD3_NODE_HOSTNAME://g" inventory/mycluster/hosts.yaml
 sed -i "/^ *$/d" inventory/mycluster/hosts.yaml
 
 sed -i "/etcd:/,/k8s_cluster:/s/$MASTER1_NODE_HOSTNAME/$ETCD1_NODE_HOSTNAME/g" inventory/mycluster/hosts.yaml
 sed -i "/etcd:/,/k8s_cluster:/s/$MASTER2_NODE_HOSTNAME/$ETCD2_NODE_HOSTNAME/g" inventory/mycluster/hosts.yaml
 sed -i "/etcd:/,/k8s_cluster:/s/$MASTER3_NODE_HOSTNAME/$ETCD3_NODE_HOSTNAME/g" inventory/mycluster/hosts.yaml
-
-declare -a IPS=($MASTER1_NODE_PRIVATE_IP $MASTER2_NODE_PRIVATE_IP $MASTER3_NODE_PRIVATE_IP $ETCD1_NODE_PRIVATE_IP $ETCD2_NODE_PRIVATE_IP $ETCD3_NODE_PRIVATE_IP $WORKER1_NODE_PRIVATE_IP $WORKER2_NODE_PRIVATE_IP $WORKER3_NODE_PRIVATE_IP)
-CONFIG_FILE=inventory/mycluster/hosts.yaml python3 contrib/inventory_builder/inventory.py ${IPS[@]}
