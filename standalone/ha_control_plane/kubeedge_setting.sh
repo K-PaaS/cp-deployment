b4e8eca91df72d7a846a6e4643e635cab76fa72c
Taewoo Kim <67407365+dev-taewoo@users.noreply.github.com>
2022-05-16 16:41:30 +0900

    Modify HA Deployment Script

--- standalone/ha_control_plane/kubeedge_setting.sh
+++ standalone/ha_control_plane/kubeedge_setting.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
+find inventory/mycluster/hosts.yaml -exec sed -i -r -e "/etcd:/i\    cloudcore_node:\n      hosts:\n        $MASTER1_NODE_HOSTNAME" {} \;
+find inventory/mycluster/hosts.yaml -exec sed -i -r -e "/etcd:/i\    edge_node:\n      hosts:\n" {} \;
+
 for ((i=0;i<$EDGE_NODE_CNT;i++))
   do
     j=$((i+1));
     eval "edge_node_hostname=\${EDGE${j}_NODE_HOSTNAME}";
     eval "edge_node_private_ip=\${EDGE${j}_NODE_PRIVATE_IP}";
-    
-	find inventory/mycluster/hosts.yaml -exec sed -i -r -e "/children:/i\    $edge_node_hostname:\n      ansible_host: $edge_node_private_ip\n      ip: $edge_node_private_ip\n      access_ip: $edge_node_private_ip" {} \;
-    find inventory/mycluster/hosts.yaml -exec sed -i -r -e "etcd:/i\    edge_node:\n      hosts:\n        $edge_node_hostname:" {} \;
+
+    find inventory/mycluster/hosts.yaml -exec sed -i -r -e "/children:/i\    $edge_node_hostname:\n      ansible_host: $edge_node_private_ip\n      ip: $edge_node_private_ip\n      access_ip: $edge_node_private_ip" {} \;
+    find inventory/mycluster/hosts.yaml -exec sed -i -r -e "/etcd:/i\        $edge_node_hostname:" {} \;
 done
 
 sed -i "s/{MASTER_NODE_PUBLIC_IP}/$MASTER1_NODE_PUBLIC_IP/g" roles/paasta-cp/edge/keadm_init/defaults/main.yml
 sed -i "s/{MASTER_NODE_PRIVATE_IP}/$MASTER1_NODE_PRIVATE_IP/g" roles/paasta-cp/edge/keadm_init/defaults/main.yml
 
 sed -i "s/{MASTER_NODE_PUBLIC_IP}/$MASTER1_NODE_PUBLIC_IP/g" roles/paasta-cp/edge/keadm_join/defaults/main.yml
+
+sed -i "s/{CLOUDCOREIPS}/$CLOUDCOREIPS/g" roles/paasta-cp/edge/cloudcore/defaults/main.yml
+
+sed -i "s/{MASTER_HOSTNAME}/$MASTER1_NODE_HOSTNAME/g" ../../edge/edgemesh/server/06-deployment.yaml
