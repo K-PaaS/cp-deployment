cd66356abaf37a2b1d6597cfc0a5d12631461424
Taewoo Kim <67407365+dev-taewoo@users.noreply.github.com>
2022-05-30 14:43:44 +0900

    Modify Deploy KubeEdge Script

--- standalone/ha_control_plane/deploy_kubeedge_external.sh
+++ standalone/ha_control_plane/deploy_kubeedge_external.sh
@@ -3,7 +3,7 @@
 sudo chmod o+w /tmp
 sudo chmod o+w /var/tmp
 
-source kubespray_var.sh
+source kubespray_var_external.sh
 source kubeedge_var.sh
 source kubeedge_setting.sh
 source kubeedge_install.sh
