c7a22e233255126cc2a8d189c64e2ec464ff3ed6
Taewoo Kim <67407365+dev-taewoo@users.noreply.github.com>
2024-04-01 16:23:57 +0900

    Modify Terraman Deployment

--- standalone/deploy-cp-cluster-terraman.sh
+++ standalone/deploy-cp-cluster-terraman.sh
@@ -6,6 +6,7 @@ source cp-cluster-terraman-vars.sh
 # Container platform configuration settings
 cp roles/kubernetes/control-plane/tasks/kubeadm-setup.yml.ori roles/kubernetes/control-plane/tasks/kubeadm-setup.yml
 cp roles/kubernetes-apps/metrics_server/defaults/main.yml.ori roles/kubernetes-apps/metrics_server/defaults/main.yml
+cp inventory/mycluster/group_vars/all/all.yml.ori inventory/mycluster/group_vars/all/all.yml
 
 sed -i "s/{MASTER1_NODE_HOSTNAME}/$MASTER1_NODE_HOSTNAME/g" roles/kubernetes-apps/metrics_server/defaults/main.yml
 sed -i "s/{MASTER1_NODE_PUBLIC_IP}/$MASTER1_NODE_PUBLIC_IP/g" roles/kubernetes/control-plane/tasks/kubeadm-setup.yml
