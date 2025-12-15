efd7177d655afc6f63b03a4d5d5c221cfa2d810d
Taewoo Kim <dev.taewookim@gmail.com>
2025-12-15 16:52:54 +0900

    Modify

---
+++ standalone/reset-cp-edge.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+OS_VERSION=$(cat /etc/lsb-release | grep DISTRIB_RELEASE | awk -F '=' '{print $2}')
+
+if [ "$OS_VERSION" == "24.04" ]; then
+  source $HOME/kpaas-venv/bin/activate
+else
+  export PATH=$PATH:$HOME/.local/bin
+  source $HOME/.bashrc
+fi
+
+ansible-playbook -i localhost, -c local playbooks/local-edge.yml
+RET=$?
+if [ "$RET" -ne 0 ]; then
+  exit 1
+else
+  ansible-playbook -i inventory/mycluster/edge-hosts.yaml -e reset_confirmation=yes --become --become-user=root playbooks/reset-edge.yml
+fi
